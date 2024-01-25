64c824694a7b4dd738cfcb7117227f0a21b5b3fd
shamama <shamamaguliyeva04@gmail.com>
2024-01-25 09:51:41 +0400

    Employee crud finished

--- Bilet15Mamba/Areas/Admin/Controllers/EmployeeController.cs
+++ Bilet15Mamba/Areas/Admin/Controllers/EmployeeController.cs
@@ -5,6 +5,7 @@ using Bilet15Mamba.Utilities.Extension;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.EntityFrameworkCore;
 using System.Data;
 
@@ -24,10 +25,10 @@ namespace Bilet15Mamba.Areas.Admin.Controllers
             _env = env;
         }
 
-        public async Task<IActionResult> Index(int page=1)
+        public async Task<IActionResult> Index(int page = 1)
         {
             double count = await _context.Employees.CountAsync();
-            List<Employee> employees = await _context.Employees.Skip((page-1)*5).Take(5).Include(x=>x.Position).ToListAsync();
+            List<Employee> employees = await _context.Employees.Skip((page - 1) * 5).Take(5).Include(x => x.Position).ToListAsync();
 
             PaginationVm<Employee> paginationVm = new PaginationVm<Employee>
             {
@@ -53,7 +54,7 @@ namespace Bilet15Mamba.Areas.Admin.Controllers
                 GetList(employeeVm);
                 return View(employeeVm);
             }
-            bool result = _context.Positions.Any(x=>x.Id==employeeVm.PositionId);
+            bool result = _context.Positions.Any(x => x.Id == employeeVm.PositionId);
             if (!result)
             {
                 ModelState.AddModelError("PositionId", "Position not found");
@@ -91,6 +92,96 @@ namespace Bilet15Mamba.Areas.Admin.Controllers
             return RedirectToAction(nameof(Index));
         }
 
+        public async Task<IActionResult> Update(int id)
+        {
+            if (id <= 0) return BadRequest();
+            var existed = await _context.Employees.FirstOrDefaultAsync(x => x.Id == id);
+            if (existed is null) return NotFound();
+
+            UpdateEmployeeVm employeeVm = new UpdateEmployeeVm
+            {
+                Name = existed.Name,
+                Surname = existed.Surname,
+                PositionId = existed.PositionId,
+                ImageUrl = existed.ImageUrl,
+                FbLink = existed.FbLink,
+                TwLink = existed.TwLink,
+                IgLink = existed.IgLink,
+                LinLink = existed.LinLink
+            };
+            GetList(employeeVm);
+            return View(employeeVm);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Update(int id, UpdateEmployeeVm employeeVm)
+        {
+            if (!ModelState.IsValid)
+            {
+                GetList(employeeVm);
+                return View(employeeVm);
+            }
+            var existed = await _context.Employees.FirstOrDefaultAsync(x => x.Id == id);
+            if (existed is null) return NotFound();
+
+            bool result = _context.Positions.Any(x => x.Id == employeeVm.PositionId);
+            if (!result)
+            {
+                ModelState.AddModelError("PositionId", "Position not found");
+                GetList(employeeVm);
+                return View(employeeVm);
+            }
+            if (employeeVm.Photo is not null)
+            {
+                if (!employeeVm.Photo.CheckType())
+                {
+                    ModelState.AddModelError("Photo", "Photo type is not valid.");
+                    GetList(employeeVm);
+                    return View(employeeVm);
+                }
+                if (!employeeVm.Photo.CheckSize())
+                {
+                    ModelState.AddModelError("Photo", "Photo size is not valid.");
+                    GetList(employeeVm);
+                    return View(employeeVm);
+                }
+                string newimage = await employeeVm.Photo.CreateFile(_env.WebRootPath, "assets", "img", "team");
+                existed.ImageUrl.DeleteFile(_env.WebRootPath, "assets", "img", "team");
+                existed.ImageUrl = newimage;
+            }
+            existed.Name = employeeVm.Name;
+            existed.Surname = employeeVm.Surname;
+            existed.PositionId = employeeVm.PositionId;
+            existed.FbLink = employeeVm.FbLink;
+            existed.IgLink = employeeVm.IgLink;
+            existed.LinLink = employeeVm.LinLink;
+            existed.TwLink = employeeVm.TwLink;
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        public async Task<IActionResult> Delete(int id)
+        {
+            if (id <= 0) return BadRequest();
+            var existed = await _context.Employees.FirstOrDefaultAsync(x => x.Id == id);
+            if (existed is null) return NotFound();
+
+            existed.ImageUrl.DeleteFile(_env.WebRootPath, "assets", "img", "team");
+
+            _context.Remove(existed);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        public async Task<IActionResult> Detail(int id)
+        {
+            if (id <= 0) return BadRequest();
+            var existed = await _context.Employees.Include(x=>x.Position).FirstOrDefaultAsync(x => x.Id == id);
+            if (existed is null) return NotFound();
+
+            return View(existed);
+        }
 
         private void GetList(CreateEmployeeVm employeeVm)
         {
