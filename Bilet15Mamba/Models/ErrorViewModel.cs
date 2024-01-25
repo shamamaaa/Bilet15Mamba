ce2837ce55e4a35448459f13e02589a9a7d91fe7
shamama <shamamaguliyeva04@gmail.com>
2024-01-25 08:14:46 +0400

    Initial

---
+++ Bilet15Mamba/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Bilet15Mamba.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
